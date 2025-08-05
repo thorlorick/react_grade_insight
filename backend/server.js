6abe203d306a61d2783cf49212a6461cfe594a7c
thorlorick <thorlorick@gmail.com>
2025-08-05 10:25:11 -0400

    version 1 of code-

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-need code
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Grade Insight backend running');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
