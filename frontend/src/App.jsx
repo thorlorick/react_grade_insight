5c0dbdf2f868b4a21f42427ad62b0eb146c0f890
ThorloRick <thorlorick@gmail.com>
2025-08-15 17:25:29 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,12 @@
-need code-
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Grade Insight</h1>
+      <p>Your app is running!</p>
+    </div>
+  );
+}
+
+export default App;
