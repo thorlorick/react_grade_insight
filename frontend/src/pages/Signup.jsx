759d64cedf57b6fe98d0e011cc83c46c1095394e
ThorloRick <thorlorick@gmail.com>
2025-08-19 12:44:08 -0400

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,13 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Gradeinsight.module.css';
 
-const Login = () => {
-  const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
-
-  const handleButtonClick = (e) => {
-    e.preventDefault();
-    setIsLoginFormVisible(true);
-  };
+const Signup = () => {
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -36,15 +31,8 @@ const Login = () => {
         <div className={styles.overlay}></div>
         <div className={styles.content}>
           <div className={styles.contentInner}>
-            <h1 className={`${styles.heroText} ${isLoginFormVisible ? styles.fadeOut : ''}`}>
-              Grade Insight.<br />
-              When<br />
-              Good isn't<br />
-              Enough.
-            </h1>
-            
-            <div className={`${styles.loginForm} ${isLoginFormVisible ? styles.fadeIn : ''}`}>
-              <h2 className={styles.loginTitle}>Your Email and Password, please...</h2>
+            <div className={`${styles.loginForm} ${styles.fadeIn}`}>
+              <h2 className={styles.loginTitle}>Please Enter your Information</h2>
               <div className={styles.formContainer}>
                 <div className={styles.formGroup}>
                   <input
@@ -60,24 +48,25 @@ const Login = () => {
                     placeholder="Password"
                   />
                 </div>
+
+                  <div className={styles.formGroup}>
+                  <input
+                    type="text"
+                    className={styles.formInput}
+                    placeholder="Student's Email"
+                  />
+                </div>
                 
                 <button
                   onClick={handleFormSubmit}
                   className={styles.loginButton}>
-                  Secure Sign In
+                  Register
                 </button>
-                <div className={styles.signupLink}>
-                  NOT SURE WHAT TO DO?<br /> <Link to="/signup" className={styles.signupLinkAnchor}>CLICK HERE</Link><br />
-                </div>
+                
               </div>
             </div>
             
-            <button
-              onClick={handleButtonClick}
-              className={`${styles.adventureButton} ${isLoginFormVisible ? styles.fadeOut : ''}`}
-            >
-              See Your Grades
-            </button>
+            <
           </div>
         </div>
       </div>
@@ -85,4 +74,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
