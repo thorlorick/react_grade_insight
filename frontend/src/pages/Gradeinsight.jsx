081d75b396dbcfea73c1f52776b16cb04f286648
ThorloRick <thorlorick@gmail.com>
2025-08-18 21:22:31 -0400

    Update Gradeinsight.jsx

--- frontend/src/pages/Gradeinsight.jsx
+++ frontend/src/pages/Gradeinsight.jsx
@@ -55,8 +55,7 @@ const Login = () => {
                   Secure Sign In
                 </button>
                 <div className={styles.signupLink}>
-                  Need a Teacher account?<a href="#" className={styles.signupLinkAnchor}>  Purchase Here</a><br />
-               
+                  Need a Teacher account?<br /> <a href="#" className={styles.signupLinkAnchor}>Purchase Here</a><br />
                 </div>
               </div>
             </div>
