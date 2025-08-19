59bd1388d192d386265885e32ed27acb53a6b186
ThorloRick <thorlorick@gmail.com>
2025-08-19 08:48:49 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Uploads from './pages/Uploads';
 import Gradeinsight from './pages/Gradeinsight';
 import Login from './pages/Login';
+import Signup from './pages/Signup'; 
 
 
 const App = () => {
