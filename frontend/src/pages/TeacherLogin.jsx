26faa33dc0c6c56bb1a36c2401deda5231f23b29
ThorloRick <thorlorick@gmail.com>
2025-08-22 18:58:00 -0400

    syntax rewind

--- frontend/src/pages/TeacherLogin.jsx
+++ frontend/src/pages/TeacherLogin.jsx
@@ -3,25 +3,41 @@ import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import BackgroundContainer from '../components/BackgroundContainer';
 import LoginContainer from '../components/LoginContainer';
+import loginStyles from './TeacherLogin.module.css';
 
 const Login = () => {
   return (
-    <div className={body}>
+    <div className={loginStyles.body}>
       <Navbar />
       <BackgroundContainer image="/images/insightBG.jpg">
         <LoginContainer title="Teacher Login">
-          <div className="formGroup">
-            <label className="formLabel">Username</label>
-            <input className="formInput" type="text" placeholder="Username" />
+          <div className={loginStyles.formGroup}>
+            <label className={loginStyles.formLabel}>Username</label>
+            <input
+              className={loginStyles.formInput}
+              type="text"
+              placeholder="Username"
+            />
           </div>
-          <div className="formGroup">
-            <label className="formLabel">Password</label>
-            <input className="formInput" type="password" placeholder="Password" />
+
+          <div className={loginStyles.formGroup}>
+            <label className={loginStyles.formLabel}>Password</label>
+            <input
+              className={loginStyles.formInput}
+              type="password"
+              placeholder="Password"
+            />
           </div>
-          <button className="loginButton" type="submit">Log In</button>
-          <br />
-          <div className="signupLink">
-            Don't have an account? <Link to="/signup" className="signupLinkAnchor">Sign up here</Link>
+
+          <button className={loginStyles.loginButton} type="submit">
+            Log In
+          </button>
+
+          <div className={loginStyles.signupLink}>
+            Don't have an account?{' '}
+            <Link to="/signup" className={loginStyles.signupLinkAnchor}>
+              Sign up here
+            </Link>
           </div>
         </LoginContainer>
       </BackgroundContainer>
