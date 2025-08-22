d206ccf60d560039eb344d745aa5841bc25f5b9d
ThorloRick <thorlorick@gmail.com>
2025-08-22 18:28:11 -0400

    syntax

--- frontend/src/pages/TeacherLogin.jsx
+++ frontend/src/pages/TeacherLogin.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import BackgroundContainer from '../components/BackgroundContainer';
 import LoginContainer from '../components/LoginContainer';
