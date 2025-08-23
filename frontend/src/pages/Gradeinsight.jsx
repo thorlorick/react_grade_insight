0dc7f622ac8fbb7a31acbc7ae6225503aa75c4b9
ThorloRick <thorlorick@gmail.com>
2025-08-22 20:17:16 -0400

    containers

--- frontend/src/pages/Gradeinsight.jsx
+++ frontend/src/pages/Gradeinsight.jsx
@@ -1,52 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import BackgroundContainer from '../components/BackgroundContainer';
+import LoginContainer from '../components/LoginContainer';
 import styles from './Gradeinsight.module.css';
 
-const Gradeinsight = () => {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted!');
-  };
-
-  return (
+const GradeInsight = () => {
+ return (
     <div className={styles.body}>
-      <nav className={styles.navbar}>
-        <Link to="/" className={styles.navLogo}>Grade Insight</Link>
-         <div className={styles.navLinks}>
-          <a href="/teacherLogin" className={styles.navLink}>Teacher</a>
-          <a href="#" className={styles.navLink}>Student</a>
-          <a href="#" className={styles.navLink}>Parent</a>
-          <a href="#" className={styles.navLink}>Contact</a>
-        </div>
-      </nav>
-
-      <div
-        className={styles.heroContainer}
-        style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/insightBG.jpg)` }}
-      >
-        <div className={styles.overlay}></div>
-        <div className={styles.content}>
-          <div className={styles.contentInner}>
-            <h1 className={styles.heroText}>
-              Grade Insight.<br />
-              When Good Isn't Enough.
-            </h1>
-
-            <div className={styles.loginForm}>
-              <form onSubmit={handleFormSubmit}>
-                <div className={styles.formContainer}>
-                  <div className={styles.formGroup}>
-                    {/* Input fields for username, password, etc. */}
-                  </div>
-                </div>
-                
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Navbar />
+      <BackgroundContainer image="/images/insightBG.jpg">
+        <LoginContainer title="Grade Insight.">
+        <LoginContainer title="When GOOD isn't enough."></LoginContainer>
+          
+        </LoginContainer>
+      </BackgroundContainer>
     </div>
   );
 };
 
-export default Gradeinsight;
+export default GradeInsight;
