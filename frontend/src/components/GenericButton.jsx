f771812d11d9ea1f6be51948abae13e3e620f353
ThorloRick <thorlorick@gmail.com>
2025-08-25 15:56:35 -0400

    code in

--- frontend/src/components/GenericButton.jsx
+++ frontend/src/components/GenericButton.jsx
@@ -1,1 +1,17 @@
-/* code here */
+import React from "react";
+import styles from "./GenericButton.module.css";
+
+const GenericButton = ({ label, onClick, type = "button", variant = "primary" }) => {
+  return (
+    <button
+      type={type}
+      className={`${styles.button} ${styles[variant]}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default GenericButton;
+
