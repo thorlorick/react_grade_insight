4d1e7ffd95367505ebf4778d9d1dd91fe7ea62ff
ThorloRick <thorlorick@gmail.com>
2025-08-25 21:19:34 -0400

    design

--- frontend/src/components/GenericButton.jsx
+++ frontend/src/components/GenericButton.jsx
@@ -1,17 +1,13 @@
+// src/components/GenericButton.jsx
 import React from "react";
 import styles from "./GenericButton.module.css";
 
-const GenericButton = ({ label, onClick, type = "button", variant = "primary" }) => {
+const GenericButton = ({ onClick, children, type = "button" }) => {
   return (
-    <button
-      type={type}
-      className={`${styles.button} ${styles[variant]}`}
-      onClick={onClick}
-    >
-      {label}
+    <button className={styles.button} onClick={onClick} type={type}>
+      {children || "Click Me"} {/* Default text if no children */}
     </button>
   );
 };
 
 export default GenericButton;
-
