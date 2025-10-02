b6f57053c725f9a3b4f4493a9c955f1ca1dc540c
ThorloRick <thorlorick@gmail.com>
2025-10-02 08:35:30 -0400

    Update StudentPage.jsx

--- frontend/src/pages/StudentPage.jsx
+++ frontend/src/pages/StudentPage.jsx
@@ -95,7 +95,7 @@ const StudentPage = () => {
       disableBeacon: true,
     },
     {
-      target: `.${styles.tableSection}`,
+      target: `.${styles.pageWrapper}`,
       content: 'Here is your assignments table. You can see grades and sort columns.',
     },
     {
