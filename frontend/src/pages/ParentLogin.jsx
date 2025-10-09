df17008e9e94fd9e88e019f305c9831101081d86
ThorloRick <thorlorick@gmail.com>
2025-10-09 13:45:09 -0400

    Update ParentLogin.jsx

--- frontend/src/pages/ParentLogin.jsx
+++ frontend/src/pages/ParentLogin.jsx
@@ -121,8 +121,7 @@ const ParentLogin = () => {
         links={[
           { to: '/TeacherLogin', label: 'Teachers' },
           { to: '/StudentLogin', label: 'Students' },
-          { to: '/ParentLogin', label: 'Parents' },
-          { to: '/contact', label: 'Contact Us' }
+          { to: '/ParentLogin', label: 'Parents' }
         ]}
       />
       <BackgroundContainer image="/images/insightBG.jpg">
