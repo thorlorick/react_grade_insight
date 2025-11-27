2580a790e9887f5f0f5d6e402bd6c098a6111dee
ThorloRick <thorlorick@gmail.com>
2025-11-27 13:46:35 -0500

    Delete backend/src/services/rick/memoryService.js

--- backend/src/services/rick/memoryService.js
+++
@@ -1,6 +0,0 @@
-// backend/services/rick/memoryService.js
-
-// Stub service: no memories, real-time only
-const getMemories = async () => ({ success: true, memories: [] });
-
-module.exports = { getMemories };
